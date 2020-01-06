214298a2e631ef999f2a2ab62c21b6deec761205
古家歌織 <furuyakaori@kaori-f.local>
2020-01-06 18:44:42 +0900

    9finish

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '0f83832642bd90795f11e63d4a1e20a17e96cbc708ee35648452f56e811d52c66a1643302e152b4d1c6535cb89debd8f3d1de7ab75eebe5a6e1b97dc87739357'
